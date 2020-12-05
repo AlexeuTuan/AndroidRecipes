b7d8b26636cbcd2d3f89660651bdd394f1c3302c
AlexeyTyan <alekseytyan45@gmail.com>
2020-12-05 18:02:43 +0300

    Splash Screen. 3 different ways to show a start screen

---
+++ app/src/main/java/com/example/apps/splashscreen/SplashDialog.java
@@ -0,0 +1,14 @@
+package com.example.apps.splashscreen;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashDialog extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO show a dialog
+    }
+}
